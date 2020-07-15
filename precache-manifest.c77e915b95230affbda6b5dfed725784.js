0b81d097265f0a67e3f0344259a2c15022816732
vitaliidemidov <demidov1@i.ua>
2020-07-15 03:11:37 +0300

    Updates

---
+++ precache-manifest.c77e915b95230affbda6b5dfed725784.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a085817c4e2327098b72fe1fb3897e",
+    "url": "/quizReactApp/index.html"
+  },
+  {
+    "revision": "9dcfba1d6a2b558c44d0",
+    "url": "/quizReactApp/static/css/main.81baa537.chunk.css"
+  },
+  {
+    "revision": "d318a9b23e2857ca1ef6",
+    "url": "/quizReactApp/static/js/2.fe11eae1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quizReactApp/static/js/2.fe11eae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dcfba1d6a2b558c44d0",
+    "url": "/quizReactApp/static/js/main.a0021d20.chunk.js"
+  },
+  {
+    "revision": "65b0dcd4c6f1d61c5a6b",
+    "url": "/quizReactApp/static/js/runtime-main.e6d85986.js"
+  }
+]);
